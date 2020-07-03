124ac45afc20d24855f912ff0f22725ef5968b6c
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-07-03 17:12:56 -0400

    code commit

---
+++ ConditionalStatements/ConditionalStatements.js
@@ -0,0 +1,12 @@
+/**
+ program where I practice conditional statmenets
+ */
+
+//sample conditional statements
+let example = true;
+
+if(example){
+	console.log('the example is set to true');
+}else{
+	console.log('the example is set to false');
+}
